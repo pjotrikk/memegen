94c6a45411e5ac7ac65ea164efdcb1c200226596
pjotrikk <jankovicpatrikk@gmail.com>
2022-10-19 13:59:01 +0200

    zaciatok

---
+++ src/components/header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HeaderSC from "./Header.styled";
+
+const Header = () => {
+  return <HeaderSC>{children}</Header>;
+};
+
+export default Header;
---
+++ src/components/header/Header.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const HeaderSC = styled.div`
+  width: 550px;
+  height: 65px;
+`;
+
+export default HeaderSC;
